2462e4a1c5d4319aa0dd7c969d60642f7202a30c
ej9374 <109258160+ej9374@users.noreply.github.com>
2025-07-29 16:38:39 +0900

    setting

---
+++ src/main/java/ddg/walking_rabbit/WalkingRabbitApplication.java
@@ -0,0 +1,13 @@
+package ddg.walking_rabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkingRabbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkingRabbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ddg/walking_rabbit/WalkingRabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package ddg.walking_rabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkingRabbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
