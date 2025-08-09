716d33815ef299ad5baea2eb773bf9378fd4500a
ej9374 <109258160+ej9374@users.noreply.github.com>
2025-08-10 02:27:31 +0900

    refactor: controller 경로 수정

--- src/main/java/ddg/walking_rabbit/user/controller/UserController.java
+++ src/main/java/ddg/walking_rabbit/user/controller/UserController.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
 
