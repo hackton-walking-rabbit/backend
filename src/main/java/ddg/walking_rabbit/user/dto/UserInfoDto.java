0bfa96e7bb958b1218a75b5426f68d322d69e5c4
ej9374 <109258160+ej9374@users.noreply.github.com>
2025-08-25 03:45:46 +0900

    feat: 유저의 마이페이지

---
+++ src/main/java/ddg/walking_rabbit/user/dto/UserInfoDto.java
@@ -0,0 +1,11 @@
+package ddg.walking_rabbit.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserInfoDto {
+    private String nickname;
+    private String profileImageUrl;
+}
